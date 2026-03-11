44d8ef4d960aae2a9061e9d65346052106867707
Madhuvarshini-Venkatachalam <madhuvarshinivenkatachalam@gmail.com>
2026-03-11 22:05:32 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -5,6 +5,7 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Palindrome Checker Application");
+        System.out.println("This application checks whether a word is palindrome or not.");
 
     }
 
