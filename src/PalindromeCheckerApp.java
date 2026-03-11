dfc985105be957c4705ba8950c5f930388b62c8b
Madhuvarshini-Venkatachalam <madhuvarshinivenkatachalam@gmail.com>
2026-03-11 22:13:13 +0530

    UC2: Print a hardcoded palindrome result

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,8 +4,11 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Palindrome Checker Application");
-        System.out.println("This application checks whether a word is palindrome or not.");
+        String word = "madam";
+
+        if(word.equals("madam")) {
+            System.out.println(word + " is a palindrome");
+        }
 
     }
 
